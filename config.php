1fae321daf8b7f3f443b89a27ffd0b1cbe8788a6
hiranrusiru2006 <166051605+hiranrusiru2006@users.noreply.github.com>
2025-05-06 21:09:04 -0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+
+### 3. `config.sample.php`
+```php
+<?php
+// SMS Gateway credentials
+// RENAME THIS FILE TO config.php AFTER SETUP
+
+define('SMS_GATEWAY_URL', 'https://www.textit.biz/sendmsg/');
+define('SMS_SENDER_ID', 'YOUR_NUMBER_HERE'); // e.g. 94769343928
+define('SMS_PASSWORD', 'YOUR_PASSWORD_HERE'); // e.g. 9694
